60ee9324e7f5721844142f6d43b072edb61c4de4
Max Andreev <Maksim_Andreyeu@epam.com>
2022-07-17 17:19:30 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function start() {
@@ -13,6 +15,20 @@ async function start() {
         transform: true,
       }),
     );
+
+    const config = new DocumentBuilder()
+      .setTitle('Service NodeJS')
+      .setDescription('The service API description')
+      .setVersion('1.0.0')
+      .addTag('service')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    writeFileSync('./doc/swagger.json', JSON.stringify(document));
+
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(PORT);
     console.log(`Application is running on: ${await app.getUrl()}`);
   } catch (e) {
